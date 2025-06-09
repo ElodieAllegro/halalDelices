ebba65dcea10730c91f01b8caf6ae4e5dcd66562
ElodieAllegro <124352648+ElodieAllegro@users.noreply.github.com>
2025-06-09 12:50:28 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/halalDelices/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
